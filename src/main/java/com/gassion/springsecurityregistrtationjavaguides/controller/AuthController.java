c73cbc0972b788958d2ed18a719d1a079becdb34
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-26 22:31:36 +0300

    feat : add registration controller

--- src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
+++ src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
@@ -1,12 +1,26 @@
 package com.gassion.springsecurityregistrtationjavaguides.controller;
 
 import com.gassion.springsecurityregistrtationjavaguides.dto.UserDTO;
+import com.gassion.springsecurityregistrtationjavaguides.entity.User;
+import com.gassion.springsecurityregistrtationjavaguides.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/index")
     public String home() {
@@ -16,5 +30,23 @@ public class AuthController {
     @GetMapping("/register")
     public String showRegistrationPage(@ModelAttribute("user") UserDTO user) {
         return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult result, Model model){
+        User existingUser = userService.findUserByEmail(userDTO.getEmail());
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDTO);
+            return "/register";
+        }
+
+        userService.saveUser(userDTO);
+        return "redirect:/register?success";
     }
 }
