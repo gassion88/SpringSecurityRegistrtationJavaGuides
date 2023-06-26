31bafe94573fe573975c5bf4c0f669c6288443d3
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-26 22:41:04 +0300

    feat : add login

--- src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
+++ src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
@@ -58,4 +58,9 @@ public class AuthController {
         model.addAttribute("users", users);
         return "users";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
