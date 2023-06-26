d20278d4ae90ecf7e5a04e9bfc4b1fa375e6adde
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-26 22:25:24 +0300

    feat : add register page controller

--- src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
+++ src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.gassion.springsecurityregistrtationjavaguides.controller;
 
+import com.gassion.springsecurityregistrtationjavaguides.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AuthController {
@@ -9,5 +11,10 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage(@ModelAttribute("user") UserDTO user) {
+        return "register";
     }
 }
