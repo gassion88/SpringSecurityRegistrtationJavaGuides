5d4980acf865fab4a0d78e20377c44a23db131f3
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-26 21:14:17 +0300

    init :

---
+++ src/main/java/com/gassion/springsecurityregistrtationjavaguides/SpringSecurityRegistrtationJavaGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.gassion.springsecurityregistrtationjavaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRegistrtationJavaGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRegistrtationJavaGuidesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gassion/springsecurityregistrtationjavaguides/SpringSecurityRegistrtationJavaGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gassion.springsecurityregistrtationjavaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRegistrtationJavaGuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
