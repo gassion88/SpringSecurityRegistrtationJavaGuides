97dc6bc871a38e3570d878179992f5855bea1d62
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-26 22:32:12 +0300

    feat :

--- src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
+++ src/main/java/com/gassion/springsecurityregistrtationjavaguides/controller/AuthController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AuthController {
 
@@ -48,5 +50,12 @@ public class AuthController {
 
         userService.saveUser(userDTO);
         return "redirect:/register?success";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model){
+        List<UserDTO> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
     }
 }
